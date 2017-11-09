837488f5e64d2dfa288dacab4d1bce67f96ef825
mtw290 <mtw290@gwu.edu>
2017-11-09 14:54:51 -0500

    Added hot zone

--- final_kershaw.R
+++ final_kershaw.R
@@ -10,5 +10,6 @@ kershaw16 <- get_pitches("mlb-clayton-kershaw", 2016)
 kershaw17 <- get_pitches("mlb-clayton-kershaw", 2017)
 
 source("savant_data.R")
+source("hot_zones.R")
 
-write.csv(new_kershaw, "current_kershaw.csv")
+write.csv(pitchDat2, "current_kershaw.csv")
